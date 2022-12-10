b6af90523476fd87278f8ef71fe8927549265bfe
Carlos Hermann Schutz Foerste <carloshsf@usp.br>
2022-12-10 00:11:53 -0300

    comentários adicionados
    primeira versão de comentários nos arquivos README.md e pratica6.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SEL0337
 Prática 6
+
+Nesta prática desenvolveu-se um script que realiza foto e vídeo atvavés do módulo de câmera acoplada à raspberry pi, além de acessar os dados da estação climática da UFSC. 
+
+Para o acesso à câmera, é necessário a importação da biblioteca picamera. Assim, torna-se possível a rotação da imagem; o acionamento da câmera; registro de texto na imagem; a captura de imagem ou registro de um vídeo; e encerramento da câmera. Tendo em vista que a imagem sofre interferência da irradiação, então adiciona-se um delay na captura para corrigir a posição da câmera. Para isso, utiliza-se a função sleep() da biblioteca time.  
+
+Os dados climáticos são obtidos através do ambiente Web Oracle URL, junto da ID 966583. Para isso, utiliza-se a função get(), responsavel pela coleta dos dados em Java, convertendo-os em python. Por fim, imprime os dados climáticos no terminal através da função pprint(). P
+
+Para mais detalhes, acessar o script "pratica6.py".
--- pratica6.py
+++ pratica6.py
@@ -1,24 +1,46 @@
+#Importa bibliotecas necessarias
+#biblioteca picamera possui funçoes úteis de manipulação de imagem 
 from picamera import PiCamera, Color
+#get() acessa os dados climaticos em java
 from requests import get 
+#sleep() possibilita delay na captura de imagem 
 from time import sleep
+#pprint() imprime os dados coletados od URL 
 from pprint import pprint
+#Módulo json extrai dados relevantes
 import json
 
+#Configuração da camera
 camera=PiCamera()
+#Rotaciona camera em 180º
 camera.rotation = 180
+#Inicia a visualização da camera
 camera.start_preview()
+#Registra os NUSP no display da camera 
 camera.annotate_text = "11352737 e 11279728"
+#Conta 5s antes da captura 
 sleep(5)
+#Captura a imagem 
 camera.capture('/home/sel/SEL0337/carlosejoao.jpg')
+#Encerra a visualização da camera
 camera.stop_preview()
 
+#Inicia a visualização da camera
 camera.start_preview()
+#Registra os NUSP no display da camera
 camera.annotate_text = "11352737 e 11279728"
+#Inicia a gravação
 camera.start_recording('/home/sel/SEL0337/videocarlosejoao.h264')
+#conta 5s
 sleep(5)
+#Encerra a gravação
 camera.stop_recording()
+#Encerra a visualização da camera
 camera.stop_preview()
 
+#URL da Oracle com o ID da UFSC
 weather = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/966583'
+#Acessa os dados climaticos 
 my_weather = get(weather).json()['items']
+#Imprime os dados climasticos no terminal 
 pprint(my_weather)
